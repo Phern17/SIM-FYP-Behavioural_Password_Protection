b2ed3614faf0e7aa86a46276196a5b063840473c
Phern17 <mphern17@gmail.com>
2020-03-22 20:24:21 +0800

    Setting up frontend, incoming connection to backend

---
+++ src/Shared/components/FormElements/Input.js
@@ -0,0 +1,87 @@
+import React, { useReducer, useEffect } from "react";
+import {validate} from "../../util/validators";
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators)
+      };
+    
+    case "TOUCH":
+      return{
+        ...state,
+        isTouched: true
+      } 
+
+    default:
+      return state;
+  }
+};
+
+const Input = props => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: props.initalValue || "",
+    isTouched: false,
+    isValid: props.intialValid || false
+  });
+
+  const {id, onInput} = props;
+  const {value, isValid} = inputState;
+
+  useEffect(() => {onInput(id, value, isValid)}, [id, value, isValid, onInput])
+
+  //to validate the value user enter into the specific <input> and 
+  //update the state of it
+  const changeHandler = event => {
+    dispatch({
+      type: 'CHANGE',
+      val: event.target.value,
+      validators: props.validator 
+    })
+  }
+
+  //to check if that specific <input> is touched by user or not and
+  //update the state of it
+  const touchHandler = event => {
+    dispatch({
+      type: 'TOUCH'
+    })
+  }
+
+  //to determine which type of input user would like by specifying 
+  //props.element. Normal input if it's "input" otherwise "textarea"
+  const element =
+    props.element === "input" ? (
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    ) : (
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    );
+
+  //error text won't show up if inputState.isValid = true
+  //and inputState.touchHandler = true;
+  return (
+    <div>
+      <label htmlFor={props.id}>{props.label}</label>
+      {element}   
+  {!inputState.isValid && inputState.touchHandler && <p>{props.errorText}</p>}
+    </div>
+  );
+};
+
+export default Input;
