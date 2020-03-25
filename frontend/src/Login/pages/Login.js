19ada8a23fc96edfa214c506b32422779af55c1a
Phern17 <mphern17@gmail.com>
2020-03-26 01:59:32 +0800

    Loading Screen added

--- frontend/src/Login/pages/Login.js
+++ frontend/src/Login/pages/Login.js
@@ -1,74 +1,89 @@
-import React ,{useContext} from 'react';
-import Input from '../../Shared/components/FormElements/Input'
-import { useForm } from "../../Shared/hooks/formhook"
-import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../Shared/util/validators';
-import { AuthContext } from '../../Shared/components/context/AuthContext';
+import React, { useContext, useState } from "react";
+import Input from "../../Shared/components/FormElements/Input";
+import { useForm } from "../../Shared/hooks/formhook";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE
+} from "../../Shared/util/validators";
+import { AuthContext } from "../../Shared/components/context/AuthContext";
+//import ErrorModal from "../../Shared/components/UI-Element/ErrorModal"
+import LoadingSpinner from "../../Shared/components/UI-Element/LoadingSpinner"
 
 const Login = () => {
+  const auth = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
+  //const [error, setError] = useState();
 
-   const auth = useContext(AuthContext) 
+  const [formState, inputHandler] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false
+      },
+      password: {
+        value: "",
+        isValid: false
+      }
+    },
+    false
+  );
 
-    const [formState, inputHandler] = useForm({
-        email : {
-            value: '',
-            isValid: false
+  const authSubmitHandler = async event => {
+    event.preventDefault();
+
+    setIsLoading(true);
+    try {
+      const response = await fetch("http://localhost:5000/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
         },
-        password: {
-            value: '',
-            isValid: false
-        }
+        body: JSON.stringify({
+          email: formState.inputs.email.value,
+          password: formState.inputs.password.value
+        })
+      });
 
-    }, false); 
+      const responseData = await response.json();
 
-    const authSubmitHandler = async event => {
-        event.preventDefault();
-        
-        try {
-          const response = await fetch("http://localhost:5000/api/users/login", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-              
-              email: formState.inputs.email.value,
-              password: formState.inputs.password.value
-            })
-          });
-    
-          const responseData = await response.json();
-          
-          console.log(responseData);
-        } catch (err) {
-          console.log(err);
-        } 
-        console.log(formState)
-        auth.login();
-      };
-    
-    return (
-      <form onSubmit={authSubmitHandler}>
-        <Input
-          element = "input"
-          id = "email"
-          type = "email"
-          label = "Email"
-          errorText = "Wrong email, please try again."
-          validator = {[VALIDATOR_REQUIRE()]}
-          onInput = {inputHandler}
-        />
-        <Input
-          element = "input"
-          id = "password"
-          type = "password"
-          label = "Password"
-          errorText = "Wrong password, please try again."
-          validator = {[VALIDATOR_MINLENGTH(5)]}
-          onInput = {inputHandler}
-        />
-        <button type="Submit" disabled={!formState.isValid}>Login</button>
-      </form>
-    );
-}
+      console.log(responseData);
+      setIsLoading(false);
+      auth.login();
+
+    } catch (err) {
+
+      console.log(err);
+      setIsLoading(false);
+      //setError(err.message || "Something went wrong, please try again later.");
+    }
+  };
+
+  return (
+    <form onSubmit={authSubmitHandler}>
+      {isLoading && <LoadingSpinner asOverlay />}
+      <Input
+        element="input"
+        id="email"
+        type="email"
+        label="Email"
+        errorText="Wrong email, please try again."
+        validator={[VALIDATOR_REQUIRE()]}
+        onInput={inputHandler}
+      />
+      <Input
+        element="input"
+        id="password"
+        type="password"
+        label="Password"
+        errorText="Wrong password, please try again."
+        validator={[VALIDATOR_MINLENGTH(5)]}
+        onInput={inputHandler}
+      />
+      <button type="Submit" disabled={!formState.isValid}>
+        Login
+      </button>
+    </form>
+  );
+};
 
 export default Login;
--- frontend/src/SignUp/pages/SignUp.js
+++ frontend/src/SignUp/pages/SignUp.js
@@ -1,91 +1,105 @@
-import React, {useContext} from 'react';
-import Input from '../../Shared/components/FormElements/Input'
-import { useForm } from "../../Shared/hooks/formhook"
-import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../Shared/util/validators';
-import {AuthContext} from "../../Shared/components/context/AuthContext"
+import React, { useContext, useState } from "react";
+import Input from "../../Shared/components/FormElements/Input";
+import { useForm } from "../../Shared/hooks/formhook";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE
+} from "../../Shared/util/validators";
+import { AuthContext } from "../../Shared/components/context/AuthContext";
 import { Button } from "react-bootstrap";
-import  "./SignUp.css"
+import "./SignUp.css";
+import LoadingSpinner from "../../Shared/components/UI-Element/LoadingSpinner";
 
 const SignUp = () => {
-    const [formState, inputHandler] = useForm({
-        name : {
-            value: '',
-            isValid: false
+  const [formState, inputHandler] = useForm(
+    {
+      name: {
+        value: "",
+        isValid: false
+      },
+
+      email: {
+        value: "",
+        isValid: false
+      },
+      password: {
+        value: "",
+        isValid: false
+      }
+    },
+    false
+  );
+
+  const auth = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const authSubmitHandler = async event => {
+    event.preventDefault();
+
+    try {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:5000/api/users/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
         },
-
-        email : {
-            value: '',
-            isValid: false
-        },
-        password: {
-            value: '',
-            isValid: false
-        }
-
-    }, false);
-
-    const auth = useContext(AuthContext)
-
-    const authSubmitHandler = async event => {
-        event.preventDefault();
-
-       try{
-            const response = await fetch('http://localhost:5000/api/users/signup', {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name: formState.inputs.name.value,
-                email: formState.inputs.email.value,
-                password: formState.inputs.password.value
-            })
+        body: JSON.stringify({
+          name: formState.inputs.name.value,
+          email: formState.inputs.email.value,
+          password: formState.inputs.password.value
         })
-        
-      const responseData = await response.json(); 
-    
+      });
 
-       console.log(responseData);
-    }catch(err){
-            console.log(err);
-        } 
-        console.log(formState);
-        auth.login();
-      };
-    
-    return (
-      <form onSubmit={authSubmitHandler}>
-        <Input
-          element = "input"
-          id = "name"
-          type = "text"
-          label = "Name"
-          errorText = "Empty name, please try again."
-          validator = {[VALIDATOR_REQUIRE()]}
-          onInput = {inputHandler}
-        />
-        <Input
-          element = "input"
-          id = "email"
-          type = "email"
-          label = "Email"
-          errorText = "Wrong email, please try again."
-          validator = {[VALIDATOR_REQUIRE()]}
-          onInput = {inputHandler}
-        />
-        <Input
-          element = "input"
-          id = "password"
-          type = "password"
-          label = "Password"
-          errorText = "Wrong password, please try again."
-          validator = {[VALIDATOR_MINLENGTH(5)]}
-          onInput = {inputHandler}
-        />
-        <Button type="Submit" disabled={!formState.isValid}>Sign Up</Button>
-      </form>
-    );
-}
+      const responseData = await response.json();
 
+      console.log(responseData);
+      setIsLoading(false);
+      auth.login();
+
+    } catch (err) {
+
+      console.log(err);
+      auth.login();
+    }
+    
+    
+  };
+
+  return (
+    <form onSubmit={authSubmitHandler}>
+      {isLoading && <LoadingSpinner asOverlay />}
+      <Input
+        element="input"
+        id="name"
+        type="text"
+        label="Name"
+        errorText="Empty name, please try again."
+        validator={[VALIDATOR_REQUIRE()]}
+        onInput={inputHandler}
+      />
+      <Input
+        element="input"
+        id="email"
+        type="email"
+        label="Email"
+        errorText="Wrong email, please try again."
+        validator={[VALIDATOR_REQUIRE()]}
+        onInput={inputHandler}
+      />
+      <Input
+        element="input"
+        id="password"
+        type="password"
+        label="Password"
+        errorText="Wrong password, please try again."
+        validator={[VALIDATOR_MINLENGTH(5)]}
+        onInput={inputHandler}
+      />
+      <Button type="Submit" disabled={!formState.isValid}>
+        Sign Up
+      </Button>
+    </form>
+  );
+};
 
 export default SignUp;
